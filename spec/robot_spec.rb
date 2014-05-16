150a0372600163b86633d4b34534d91830bd6acc
Andrew <ajcumine@gmail.com>
2014-05-16 16:14:07 +0100

    updated robot tests

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,20 +2,18 @@ require 'robot'
 
 describe Robot do
   context 'on initialisation' do
-    before :each do
-      let(:robot) {Robot.new('12E','RFRF')}
-    end
+    let(:robot) {Robot.new('12E','RFRF')}
 
     it 'has an initial x coordinate' do
-      expect(x_coord).to eq(1)
+      expect(robot.x_coord).to eq(1)
     end
 
     it 'has an initial y coordinate' do
-      expect(y_coord).to eq(1)
+      expect(robot.y_coord).to eq(1)
     end
-    
+
     it 'has a set of instructions' do
-      expect(instructions).to eq(['R','F','R','F'])
+      expect(robot.instructions).to eq(['R','F','R','F'])
     end
   end
 end
