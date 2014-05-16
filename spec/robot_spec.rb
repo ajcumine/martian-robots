41f4d6766bc22ffd679306d36ac5bf6c8c822fa0
Andrew <ajcumine@gmail.com>
2014-05-16 22:29:31 +0100

    added tests for forward instruction

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -20,4 +20,26 @@ describe Robot do
       expect(robot.instructions).to eq(['R','F','R','F'])
     end
   end
+
+  context 'following instructions F' do
+    it 'increases y coordinate when facing north' do
+      robot = Robot.new('12N','F')
+      expect(robot.final_y).to eq(3)
+    end
+
+    it 'decreases y coordinate when facing south' do
+      robot = Robot.new('12S','F')
+      expect(robot.final_y).to eq(1)
+    end
+
+    it 'increases x coordinate when facing east' do
+      robot = Robot.new('12E','F')
+      expect(robot.final_x).to eq(2)
+    end
+
+    it 'decreases x coordinate when facing west' do
+      robot = Robot.new('12W','F')
+      expect(robot.final_x).to eq(0)
+    end
+  end
 end
