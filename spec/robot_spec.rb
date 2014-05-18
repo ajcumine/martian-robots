27311ef51529bfa25d1a245d7c13e5c9150f7fa7
Andrew <ajcumine@gmail.com>
2014-05-18 12:12:05 +0100

    added scents test

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,7 +2,7 @@ require 'robot'
 
 describe Robot do
   context 'on initialisation' do
-    let(:robot) {Robot.new('12E','RFRF',5,6)}
+    let(:robot) {Robot.new('12E','RFRF',5,6,[[2,6,'N'],[0,2,'W']])}
 
     it 'has an initial x coordinate' do
       expect(robot.x_coord).to eq(1)
@@ -32,33 +32,37 @@ describe Robot do
       expect(robot).not_to be_lost
     end
 
-    it 'is about to be lost' do
+    it 'is able to be lost' do
       robot.lost
       expect(robot).to be_lost
+    end
+
+    it 'has scents of former robots' do
+      expect(robot.scents).to eq([[2,6,'N'],[0,2,'W']])
     end
   end
 
   context 'following instructions F' do
     it 'increases y coordinate when facing north' do
-      robot = Robot.new('12N','F',5,5)
+      robot = Robot.new('12N','F',5,5,[])
       robot.move
       expect(robot.final_y).to eq(3)
     end
 
     it 'decreases y coordinate when facing south' do
-      robot = Robot.new('12S','F',5,5)
+      robot = Robot.new('12S','F',5,5,[])
       robot.move
       expect(robot.final_y).to eq(1)
     end
 
     it 'increases x coordinate when facing east' do
-      robot = Robot.new('12E','F',5,5)
+      robot = Robot.new('12E','F',5,5,[])
       robot.move
       expect(robot.final_x).to eq(2)
     end
 
     it 'decreases x coordinate when facing west' do
-      robot = Robot.new('12W','F',5,5)
+      robot = Robot.new('12W','F',5,5,[])
       robot.move
       expect(robot.final_x).to eq(0)
     end
@@ -66,25 +70,25 @@ describe Robot do
 
   context 'following instructions R' do
     it 'when facing north changes to east' do
-      robot = Robot.new('12N','R',5,5)
+      robot = Robot.new('12N','R',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('E')
     end
 
     it 'when facing east changes to south' do
-      robot = Robot.new('12E','R',5,5)
+      robot = Robot.new('12E','R',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('S')
     end
 
     it 'when facing south changes to west' do
-      robot = Robot.new('12S','R',5,5)
+      robot = Robot.new('12S','R',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('W')
     end
 
     it 'when facing west changes to north' do
-      robot = Robot.new('12W','R',5,5)
+      robot = Robot.new('12W','R',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('N')
     end
@@ -92,32 +96,32 @@ describe Robot do
 
   context 'following instructions L' do
     it 'when facing north changes to west' do
-      robot = Robot.new('12N','L',5,5)
+      robot = Robot.new('12N','L',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('W')
     end
 
     it 'when facing east changes to north' do
-      robot = Robot.new('12E','L',5,5)
+      robot = Robot.new('12E','L',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('N')
     end
 
     it 'when facing south changes to east' do
-      robot = Robot.new('12S','L',5,5)
+      robot = Robot.new('12S','L',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('E')
     end
 
     it 'when facing west changes to south' do
-      robot = Robot.new('12W','L',5,5)
+      robot = Robot.new('12W','L',5,5,[])
       robot.move
       expect(robot.final_orientation).to eq('S')
     end
   end
 
   context 'when moving off the map' do
-    let(:robot) {Robot.new('11E','FF',2,2)}
+    let(:robot) {Robot.new('11E','FF',2,2,[])}
 
     it 'records the location from where it moved' do
       robot.move
